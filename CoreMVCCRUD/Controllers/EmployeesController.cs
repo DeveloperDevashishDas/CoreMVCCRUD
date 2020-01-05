628b8f44f88779c71daaa825e64143e4484e6ad4
Devashish <devashishdas508@gmail.com>
2020-01-05 09:45:14 +0530

    Unwanted code removed

--- CoreMVCCRUD/Controllers/EmployeesController.cs
+++ CoreMVCCRUD/Controllers/EmployeesController.cs
@@ -42,8 +42,6 @@ namespace CoreMVCCRUD.Controllers
         }
 
         // PUT: api/Employees/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
-        // more details see https://aka.ms/RazorPagesCRUD.
         [HttpPut("{id}")]
         public async Task<IActionResult> PutEmployee(int id, Employee employee)
         {
