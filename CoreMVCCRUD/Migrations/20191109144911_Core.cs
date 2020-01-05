f713f27b6b5798fa27b1d2b4905e9dc4ba1e7a21
Devashish <devashishdas508@gmail.com>
2020-01-05 09:38:20 +0530

    Dot Net Core WEB API Employeess Controller with CRUD EF added.

---
+++ CoreMVCCRUD/Migrations/20191109144911_Core.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CoreMVCCRUD.Migrations
+{
+    public partial class Core : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employee",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FullName = table.Column<string>(type: "varchar(250)", nullable: true),
+                    EmpCode = table.Column<string>(type: "varchar(10)", nullable: true),
+                    Position = table.Column<string>(type: "varchar(100)", nullable: true),
+                    OfficeLocation = table.Column<string>(type: "varchar(100)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employee", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employee");
+        }
+    }
+}
